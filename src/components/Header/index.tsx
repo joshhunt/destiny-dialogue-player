2e2996460585d350ea9348b0832ae0d85d185bca
joshhunt <josh@trtr.co>
2022-04-09 19:29:48 +0100

    about dialogue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import React, { ChangeEvent, useCallback } from "react";
+import Modal from "react-modal";
+
 import { useSearchContext } from "../../views/MainView/searchContext";
 
 import s from "./styles.module.css";
@@ -6,6 +8,8 @@ import s from "./styles.module.css";
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
   const {
     narrators,
     selectedNarrator,
@@ -32,9 +36,24 @@ const Header: React.FC<HeaderProps> = () => {
     [setSearchText]
   );
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {}
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className={s.stickyHeader}>
-      <div className={s.title}>Destiny Dialgoue Archive</div>
+      <div className={s.main}>
+        <div className={s.title}>Destiny Dialgoue Archive</div>
+        <button className={s.aboutButton} onClick={openModal}>
+          About
+        </button>
+      </div>
 
       {narrators.length > 0 && (
         <div className={s.extraItems}>
@@ -49,11 +68,7 @@ const Header: React.FC<HeaderProps> = () => {
             />
           </div>
 
-          <span className="Space" />
-          <span className="Space" />
-          <span className="Space" />
-
-          <>
+          <div>
             <label htmlFor="narrator-select">Narrator:</label>
             <select
               id="narrator-select"
@@ -68,9 +83,56 @@ const Header: React.FC<HeaderProps> = () => {
                 </option>
               ))}
             </select>
-          </>
+          </div>
         </div>
       )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        overlayClassName={s.modalOverlay}
+        portalClassName={s.modalRoot}
+        className={s.modal}
+      >
+        <h2 className={s.modalTitle}>Hello, world :) </h2>
+
+        <p className={s.desc}>
+          Destiny Dialogue Archive is a fan-made archive of Destiny's dialogue
+          from multiple versions. The intent for this site is to archive
+          dialogue as it is removed from the game, not to "datamine" or share
+          spoilers. Minimal effort has been put into keeping spoilers off the
+          site, but read at your own risk.
+        </p>
+
+        <p className={s.desc}>
+          Currently, it contains dialogue just from Destiny 3.4.0.4 - just
+          before Witch Queen. Other versions (past and future) will be added
+          later.
+        </p>
+
+        <p className={s.desc}>
+          Many thanks to montague, ginsor, Phillip, and all the others who have
+          helped on this project.
+        </p>
+
+        <p className={s.altDesc}>
+          Note: for technical reasons, the narrator sometimes might be missing,
+          or incorrect. If you have knowledge of Destiny's dialogue tables, feel
+          free to DM me and help out :)
+        </p>
+
+        <p className={s.desc}>
+          <a href="https://twitter.com/joshhunt">@joshhunt</a>
+        </p>
+
+        <button className={s.closeButton} onClick={closeModal}>
+          <span className={s.stopIcon}>
+            <i className="fa-regular fa-xmark"></i>
+          </span>
+          Close
+        </button>
+      </Modal>
     </div>
   );
 };
