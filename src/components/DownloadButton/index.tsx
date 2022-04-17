f8667c5d6fbb4f207c965a777292ed0246c0917e
joshhunt <josh@trtr.co>
2022-04-17 19:12:13 +0100

    download

--- src/components/DownloadButton/index.tsx
+++ src/components/DownloadButton/index.tsx
@@ -1,22 +1,11 @@
 import { DialogueLine } from "../../types";
 import cx from "classnames";
-import { useMemo } from "react";
 import { getMP3URL } from "../../lib/dialogueAPI";
 
 interface Props {
   node: DialogueLine;
   className?: string;
 }
-
-const slugify = (str: string) =>
-  str
-    .replace(/\s+/g, "-")
-    .replace(/[^a-z0-9-]/gi, "")
-    .toLowerCase()
-    .trim();
-
-const trim = (str: string) =>
-  str.at(-1) === "-" ? str.slice(0, str.length - 2).trim() : str.trim();
 
 export function DownloadButton({ node, className }: Props) {
   return (
