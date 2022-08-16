083bc6fac248cf3d7dc6fc8c665bb0fc8e2f23ee
joshhunt <josh@trtr.co>
2022-08-16 21:36:28 +0100

    update versions, drive from central config

--- src/components/IncludedDialogue.tsx
+++ src/components/IncludedDialogue.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { versions } from "../lib/versionMap";
 
 interface IncludedDialogueProps {}
 
 const IncludedDialogue: React.FC<IncludedDialogueProps> = () => {
   return (
     <ul>
-      <li>2.6.1.1 (Undying/Shadowkeep)</li>
-      <li>2.9.2.1 (Arrivals)</li>
-      <li>3.4.0.4 (Lost/30th Anniversary)</li>
-      <li>4.0.1.2 (Risen/Witch Queen)</li>
-      <li>4.1.0.0 (Haunted, selected dialogue)</li>
+      {Object.values(versions)
+        .filter((v) => !v.hideInVersionList)
+        .map((v, index) => (
+          <li key={index}>{v.verboseName}</li>
+        ))}
     </ul>
   );
 };
--- src/lib/versionMap.ts
+++ src/lib/versionMap.ts
@@ -1,14 +1,45 @@
-export const versionMap: Record<string, string> = {
-  "78698.19.10.18.1444.d2_rc": "Undying",
-  "87221.20.09.10.1506.d2_live": "Arrivals",
-  "101478.22.01.22.2200.v500_live.main": "30th Anniversary",
-  "104642.22.04.26.1901.v500_live.main": "Risen",
-  "105043.22.05.11.1611.v500_rc.main": "Haunted", // early
-  "107233.22.07.27.1901.v500_live.main": "Haunted", // 4150, end of season
+interface VersionSpec {
+  shortName: string;
+  verboseName: string;
+  hideInVersionList?: boolean;
+}
+
+export const versions: Record<string, VersionSpec> = {
+  "78698.19.10.18.1444.d2_rc": {
+    shortName: "Undying",
+    verboseName: "2.6.1.1 (Undying/Shadowkeep)",
+  },
+
+  "87221.20.09.10.1506.d2_live": {
+    shortName: "Arrivals",
+    verboseName: "2.9.2.1 (Arrivals)",
+  },
+
+  "101478.22.01.22.2200.v500_live.main": {
+    shortName: "Lost",
+    verboseName: "3.4.0.4 (Lost/30th Anniversary)",
+  },
+
+  "104642.22.04.26.1901.v500_live.main": {
+    shortName: "Risen",
+    verboseName: "4.0.1.2 (Risen/Witch Queen)",
+  },
+
+  // shouldn't be used anymore
+  "105043.22.05.11.1611.v500_rc.main": {
+    shortName: "Haunted",
+    verboseName: "4.1.0.0 ()",
+    hideInVersionList: true,
+  },
+
+  "107233.22.07.27.1901.v500_live.main": {
+    shortName: "Haunted",
+    verboseName: "4.1.5.0 (Haunted)",
+  },
 };
 
 export const latestBuildId = Math.max(
-  ...Object.keys(versionMap)
+  ...Object.keys(versions)
     .map((versionString) => {
       return versionString.match(/(\d+)\./)?.[1];
     })
@@ -16,6 +47,6 @@ export const latestBuildId = Math.max(
     .filter(Boolean)
 );
 
-export function getVersionName(versionId: string) {
-  return versionMap[versionId] ?? "Unknown";
+export function getVersionName(versionId: string): string {
+  return versions[versionId]?.shortName ?? "Unknown";
 }
