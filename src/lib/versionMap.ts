b379ac04bf31d7299ca268d49ec9615db66b1090
joshhunt <josh@trtr.co>
2022-08-16 21:12:12 +0100

    add new haunted version id

--- src/lib/versionMap.ts
+++ src/lib/versionMap.ts
@@ -3,7 +3,8 @@ export const versionMap: Record<string, string> = {
   "87221.20.09.10.1506.d2_live": "Arrivals",
   "101478.22.01.22.2200.v500_live.main": "30th Anniversary",
   "104642.22.04.26.1901.v500_live.main": "Risen",
-  "105043.22.05.11.1611.v500_rc.main": "Haunted",
+  "105043.22.05.11.1611.v500_rc.main": "Haunted", // early
+  "107233.22.07.27.1901.v500_live.main": "Haunted", // 4150, end of season
 };
 
 export function getVersionName(versionId: string) {
